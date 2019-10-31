106046fed60c98733b5383c322642cda427a5060
av.tehnik <av.tehnik@gmail.com>
2019-10-31 21:44:58 +0200

    add u8g2

--- Src/main.c
+++ Src/main.c
@@ -25,6 +25,7 @@
 /* USER CODE BEGIN Includes */
 #include <string.h>
 #include "bme280.h"
+#include "u8g2.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -34,7 +35,27 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-
+uint8_t u8x8_stm32_gpio_and_delay(U8X8_UNUSED u8x8_t *u8x8,
+    U8X8_UNUSED uint8_t msg, U8X8_UNUSED uint8_t arg_int,
+    U8X8_UNUSED void *arg_ptr)
+{
+  switch (msg)
+  {
+  case U8X8_MSG_GPIO_AND_DELAY_INIT:
+    HAL_Delay(1);
+    break;
+  case U8X8_MSG_DELAY_MILLI:
+    HAL_Delay(arg_int);
+    break;
+  case U8X8_MSG_GPIO_DC:
+    HAL_GPIO_WritePin(OLED_DC_GPIO_Port, OLED_DC_Pin, arg_int);
+    break;
+  case U8X8_MSG_GPIO_RESET:
+    HAL_GPIO_WritePin(OLED_RES_GPIO_Port, OLED_RES_Pin, arg_int);
+    break;
+  }
+  return 1;
+}
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -48,6 +69,7 @@ CAN_HandleTypeDef hcan;
 SPI_HandleTypeDef hspi1;
 
 /* USER CODE BEGIN PV */
+static u8g2_t u8g2;
 
 CAN_FilterTypeDef sFilterConfig;
 CAN_TxHeaderTypeDef TxHeader;
@@ -205,6 +227,51 @@ int8_t user_spi_write(uint8_t dev_id, uint8_t reg_addr, uint8_t *reg_data, uint1
     return rslt;
 }
 
+uint8_t u8x8_stm32_gpio_and_delay(U8X8_UNUSED u8x8_t *u8x8,
+    U8X8_UNUSED uint8_t msg, U8X8_UNUSED uint8_t arg_int,
+    U8X8_UNUSED void *arg_ptr)
+{
+  switch (msg)
+  {
+  case U8X8_MSG_GPIO_AND_DELAY_INIT:
+    HAL_Delay(1);
+    break;
+  case U8X8_MSG_DELAY_MILLI:
+    HAL_Delay(arg_int);
+    break;
+  case U8X8_MSG_GPIO_DC:
+    HAL_GPIO_WritePin(OLED_DC_GPIO_Port, OLED_DC_Pin, arg_int);
+    break;
+  case U8X8_MSG_GPIO_RESET:
+    HAL_GPIO_WritePin(OLED_RES_GPIO_Port, OLED_RES_Pin, arg_int);
+    break;
+  }
+  return 1;
+}
+
+uint8_t u8x8_byte_4wire_hw_spi(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int,
+    void *arg_ptr)
+{
+  switch (msg)
+  {
+  case U8X8_MSG_BYTE_SEND:
+    HAL_SPI_Transmit(&hspi1, (uint8_t *) arg_ptr, arg_int, 10000);
+    break;
+  case U8X8_MSG_BYTE_INIT:
+    break;
+  case U8X8_MSG_BYTE_SET_DC:
+    HAL_GPIO_WritePin(OLED_DC_GPIO_Port, OLED_DC_Pin, arg_int);
+    break;
+  case U8X8_MSG_BYTE_START_TRANSFER:
+    break;
+  case U8X8_MSG_BYTE_END_TRANSFER:
+    break;
+  default:
+    return 0;
+  }
+  return 1;
+}
+
 /* USER CODE END 0 */
 
 /**
@@ -288,6 +355,12 @@ int main(void)
   HAL_GPIO_WritePin(CS0_BME280_GPIO_Port, CS0_BME280_Pin, GPIO_PIN_SET);
   setup_bme280();
 
+
+  u8g2_Setup_ssd1306_128x64_noname_1(&u8g2, U8G2_R0, u8x8_byte_4wire_hw_spi,
+      u8x8_stm32_gpio_and_delay);
+  u8g2_InitDisplay(&u8g2);
+  u8g2_SetPowerSave(&u8g2, 0);
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -306,6 +379,14 @@ int main(void)
 	  HAL_CAN_AddTxMessage(&hcan, &TxHeader, TxData, &TxMailbox);
 	  HAL_Delay(2000);
 	  TxData[7] = TxData[7] + 1;
+
+	  u8g2_FirstPage(&u8g2);
+	    do
+	    {
+	      u8g2_SetFont(&u8g2, u8g2_font_ncenB14_tr);
+	      u8g2_DrawStr(&u8g2, 0, 15, "Hello World!");
+	      u8g2_DrawCircle(&u8g2, 64, 40, 10, U8G2_DRAW_ALL);
+	    } while (u8g2_NextPage(&u8g2));
 
 //	  HAL_GPIO_TogglePin(GPIOC, LED_Pin);
   }
