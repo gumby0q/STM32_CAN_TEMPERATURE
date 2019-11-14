9b14eae1c7a62b5dc0097d32a9c987c18b63b536
Anatolii <gumby@ukr.net>
2019-11-14 23:58:56 +0200

    fix timer configs

--- Src/main.c
+++ Src/main.c
@@ -578,9 +578,9 @@ static void MX_TIM1_Init(void)
 
   /* USER CODE END TIM1_Init 1 */
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 720;
+  htim1.Init.Prescaler = 1440;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim1.Init.Period = 10000;
+  htim1.Init.Period = 50000;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim1.Init.RepetitionCounter = 0;
   htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
@@ -716,36 +716,5 @@ void assert_failed(uint8_t *file, uint32_t line)
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
-
-
-
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
-{
-  if( htim->Instance == TIM1 ){
-	HAL_GPIO_TogglePin(GPIOC, LED_Pin);
-	++temperature1timeout;
-	++temperature2timeout;
-	++humidity1timeout;
-	++humidity2timeout;
-
-	if(temperature1timeout>= DATA_WAIT_TIMEOUT){
-		temperature1timeout = DATA_WAIT_TIMEOUT;
-	}
-
-	if(temperature2timeout>= DATA_WAIT_TIMEOUT){
-		temperature2timeout = DATA_WAIT_TIMEOUT;
-	}
-
-	if(humidity1timeout>= DATA_WAIT_TIMEOUT){
-		humidity1timeout = DATA_WAIT_TIMEOUT;
-	}
-
-	if(humidity2timeout>= DATA_WAIT_TIMEOUT){
-		humidity2timeout = DATA_WAIT_TIMEOUT;
-	}
-
- }
-}
-
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
